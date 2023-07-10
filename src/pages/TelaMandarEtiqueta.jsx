74b550bb785d5dff9fab343ecea8aa8e5635bfac
Nicolas Cardoso <98752585+Nicolas0786@users.noreply.github.com>
2023-07-09 23:16:13 -0300

    altera

--- src/pages/TelaMandarEtiqueta.jsx
+++ src/pages/TelaMandarEtiqueta.jsx
@@ -10,7 +10,7 @@ import { useParams } from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
 import HeaderApp from './headerApp';
 import jwtDecode from 'jwt-decode';
-
+import './Css/TelaMandarEtiqueta.css';
 
 
 
@@ -66,17 +66,7 @@ const TelaMandarEtiqueta = () =>{
 
     const filterDados = search.length > 0 ? tes.filter(dadosEtq => dadosEtq.idEtiqueta.toString().includes(search)): 0;
 
-   
-   /*async function busca (){
-        const sla = await Axios.get(`http://localhost:3000/etiqueta/buscarPorID/${ip}`, {
-            headers: {
-                'Authorization': `Bearer ${sessionStorage.getItem("token")}`
-            }
-        });
-        console.log(sla.data)
-        setInfo(sla.data);
-    }*/
-
+    
     async function alterarPreco(){
 
         try {
@@ -107,76 +97,58 @@ const TelaMandarEtiqueta = () =>{
  const et = (tes || []).map((etiqueta) => <option value={etiqueta.idEtiqueta} key={etiqueta.idEtiqueta}>{etiqueta.ipEtiqueta}</option>);
 
     return(
-        <body>
-            <header>
-                <HeaderApp/>
-            </header>
-            <main>
-                <div>
-                <Form>
-                    <Row className="mb-3">
-                            <Form.Group as={Col} md="1">
-                                <Form.Label>ID</Form.Label>
-                                <Form.Control id="id" type="number" defaultValue={etq.idProduto} disabled placeholder= "Id"/>
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="2">
-                                <Form.Label>Descrição</Form.Label>
-                                <Form.Control id="des" type="text" defaultValue={etq.descricaoProduto} disabled placeholder= "Descrição"/>
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="2">
-                                <Form.Label>Código</Form.Label>
-                                <Form.Control id="cod" type="number" defaultValue={etq.codigoEan} disabled placeholder= "Código"/>
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="1">
-                                <Form.Label>Preço</Form.Label>
-                                <Form.Control id="prec" type="number" defaultValue={etq.preco} disabled placeholder= "Preço"/>
-                            </Form.Group>
-
-                    </Row>    
+        <div className='telaMandarEtiqueta'>
+            <HeaderApp/>
+            <div className="formsMandar">
+                <Form className='formsMandarProduto'>
+                    <Form.Group as={Col} md='1'>
+                        <Form.Label>ID</Form.Label>
+                        <Form.Control id="id" type="number" defaultValue={etq.idProduto} disabled placeholder= "Id"/>
+                    </Form.Group>
+                    <Form.Group as={Col} md='4'>
+                        <Form.Label>Descrição</Form.Label>
+                        <Form.Control id="des" type="text" defaultValue={etq.descricaoProduto} disabled placeholder= "Descrição"/>
+                    </Form.Group>
+                    <Form.Group as={Col} md='3'> 
+                        <Form.Label>Código</Form.Label>
+                        <Form.Control id="cod" type="number" defaultValue={etq.codigoEan} disabled placeholder= "Código"/>
+                    </Form.Group>
+                    <Form.Group as={Col} md='3'>
+                        <Form.Label>Preço</Form.Label>
+                        <Form.Control id="prec" type="number" defaultValue={etq.preco} disabled placeholder= "Preço"/>
+                    </Form.Group>
+                </Form>    
+                <Form className='formsMandarEtiqueta'>
+                    <Form.Group as={Col} md='1'>
+                        <Form.Label>ID</Form.Label>
+                        <Form.Control id="id" type="text" defaultValue={(filterDados || []).map(fil => fil.idEtiqueta)} />
+                    </Form.Group>
+                    <Form.Group as={Col} md='4'>
+                        <Form.Label >IP</Form.Label>
+                        <FormSelect onChange ={handleChange}>
+                        <option></option>
+                            {et}
+                        </FormSelect>
+                    </Form.Group>
+                    <Form.Group as={Col} md='3'>
+                        <Form.Label>Nome Etiqueta</Form.Label>
+                        <Form.Control id="nome" type="text" defaultValue={(filterDados || []).map(fil => fil.nomeEtiqueta)} disabled />
+                    </Form.Group>
+                    <Form.Group as={Col} md='2'>
+                        <Form.Label>Corredor</Form.Label>
+                        <Form.Control id="corre" type="text" defaultValue={(filterDados || []).map(fil => fil.corredor)} disabled />
+                    </Form.Group>
+                    <Form.Group as={Col} md='2'>
+                        <Form.Label>Pratilheira</Form.Label>
+                        <Form.Control id="prati" defaultValue={(filterDados || []).map(fil => fil.pratilheira)} type="text" disabled/>
+                    </Form.Group>
                 </Form>
-            </div>       
-            <div>
-                <Form>
-                    <Row className="mb-3">
-
-                            <Form.Group as={Col} md="1">
-                                <Form.Label>ID</Form.Label>
-                                <Form.Control id="id" type="text" defaultValue={(filterDados || []).map(fil => fil.idEtiqueta)} />
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="2">
-                                <Form.Label >IP</Form.Label>
-                                <FormSelect onChange ={handleChange}>
-                                <option></option>
-                                    {et}
-                                </FormSelect>
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="2">
-                                <Form.Label>Nome Etiqueta</Form.Label>
-                                <Form.Control id="nome" type="text" defaultValue={(filterDados || []).map(fil => fil.nomeEtiqueta)} disabled />
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="2">
-                                <Form.Label>Corredor</Form.Label>
-                                <Form.Control id="corre" type="text" defaultValue={(filterDados || []).map(fil => fil.corredor)} disabled />
-                            </Form.Group>
-
-                            <Form.Group as={Col} md="2">
-                                <Form.Label>Pratilheira</Form.Label>
-                                <Form.Control id="prati" defaultValue={(filterDados || []).map(fil => fil.pratilheira)} type="text" disabled/>
-                            </Form.Group>
-
-                    </Row>
-                </Form>
-                </div>
-                <Button onClick={alterarPreco}>Salvar</Button>
-                <Button onClick={()=> navigate('/TelaProduto')}>Fechar</Button>
-            </main>
-        </body>
+            </div>
+            <div className='areaBotoesMandarEtiqueta'>
+                <Button className='btns' onClick={alterarPreco}>Salvar</Button>
+                <Button className='btns' onClick={()=> navigate('/TelaProduto')}>Fechar</Button>
+            </div>
+        </div>
     );
 
 }
